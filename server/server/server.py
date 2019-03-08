5efcf62e83ffe18d38d3c89a95e384493a13fc78
Chase <Chase@114.227.21.198.tigernet.wifi.dyn.clemson.edu>
2019-03-08 11:58:07 -0500

    Added raw SQL for add_playlist.

--- server/server/server.py
+++ server/server/server.py
@@ -6,6 +6,7 @@ from flask import Flask, request, cli, g
 from passlib.hash import hex_sha256
 from time import time
 import sqlalchemy
+from sqlalchemy.sql import text
 import datetime
 import shutil
 from operator import itemgetter
@@ -541,10 +542,17 @@ def add_playlist():
     return Response({'missing':missing},400,isError=True).end()
 
   # valid parameters, create and return it
-  newPlaylist = Playlist(user_id=user_id,title=title,description=description)
-  db.session.add(newPlaylist)
-  db.session.commit()
-  return Response(newPlaylist.to_json()).end()
+  #newPlaylist = Playlist(user_id=user_id,title=title,description=description)
+  #db.session.add(newPlaylist)
+  #db.session.commit()
+  sql = text("""INSERT INTO Playlist(user_id, title, description) VALUES(:user_id, :title, :description)""")
+  result = db.engine.execute(sql, user_id=user_id, title=title, description=description)
+  result = db.engine.execute('SELECT playlist_id,user_id,title,description FROM Playlist WHERE playlist_id={ID}'.format(ID=result.lastrowid))
+  data = get_query_data(result)
+  if data:
+    return Response(data[0]).end()
+  else:
+    return Response("Playlist creation failed",500,True).end()
 
 @app.route('/playlists/<playlist_id>',methods=['GET'])
 def get_playlist(playlist_id):
